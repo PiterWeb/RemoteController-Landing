9e771ebb4a007fb827dd95b0662237996024e3cd
PiterWeb <piterzdev@gmail.com>
2024-07-22 19:32:52 +0200

    render in different languages based on crawler

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,6 +7,27 @@ export const handle: Handle = async ({ event, resolve }) => {
 	if (lang) {
 		lang = new Intl.Locale(lang).language
 		locale.set(lang)
+		return resolve(event)
 	}
+
+	const userAgent = event.request.headers.get('user-agent')?.toLowerCase()
+
+	switch (true) {
+		case userAgent?.includes('googlebot'):
+		case userAgent?.includes('bingbot'):
+		case userAgent?.includes('yahooua'):
+			locale.set('en')
+			break
+		case userAgent?.includes('baiduspider'):
+			locale.set('zh')
+			break
+		case userAgent?.includes('yandexbot'):
+			locale.set('ru')
+		break
+		default:
+			locale.set('en')
+	}
+
 	return resolve(event)
+
 }
