08f0ae73e5b6dea77dc6cd82d06fee2a6e296b8d
PiterDev <71133634+PiterWeb@users.noreply.github.com>
2025-04-04 07:54:30 +0200

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const githubUrl = "https://github.com/PiterWeb/LibreRemotePlay";
 export const downloadUrl = `${githubUrl}/releases/latest/`;
 export const clientUrl = 'https://libreremoteplay-client.vercel.app';
-export const discordUrl = "https://discord.gg/UBzpCDPxFd";
+export const discordUrl = "https://discord.gg/v7qd7nU3fJ";
 export const wikiUrl = `${githubUrl}/wiki`;
